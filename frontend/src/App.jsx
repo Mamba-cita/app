17b45efd88f834ea7d2036facccc12a43842f1f2
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-07-20 12:56:36 +0300

    remove theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,40 @@
-import React, { useMemo } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { useSelector } from "react-redux";
-import { themeSettings } from "./theme";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Layout from "./pages/layout/index";
-import Customers from "./pages/Customers";
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function Logout() {
-  localStorage.clear();
-  return <Navigate to="/login" />;
+  localStorage.clear()
+  return <Navigate to="/login" />
 }
 
 function RegisterAndLogout() {
-  localStorage.clear();
-  return <Register />;
+  localStorage.clear()
+  return <Register />
 }
 
 function App() {
-  const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<RegisterAndLogout />} />
-          <Route
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="/" element={<Home />} />
-            <Route path="customers" element={<Customers />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,98 +1,85 @@
-import { useState, useEffect, useMemo } from "react";
-import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "../theme";
+import { useState, useEffect } from "react";
 import api from "../api";
-import Note from "../components/Note";
-import "../styles/Home.css";
+import Note from "../components/Note"
+import "../styles/Home.css"
 
-const Home = () => {
-  const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+function Home() {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
 
-  const [notes, setNotes] = useState([]);
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
+    useEffect(() => {
+        getNotes();
+    }, []);
 
-  useEffect(() => {
-    getNotes();
-  }, []);
+    const getNotes = () => {
+        api
+            .get("/api/notes/")
+            .then((res) => res.data)
+            .then((data) => {
+                setNotes(data);
+                console.log(data);
+            })
+            .catch((err) => alert(err));
+    };
 
-  const getNotes = () => {
-    api
-      .get("/api/notes/")
-      .then((res) => {
-        setNotes(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => alert(err.message));
-  };
+    const deleteNote = (id) => {
+        api
+            .delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert("Note deleted!");
+                else alert("Failed to delete note.");
+                getNotes();
+            })
+            .catch((error) => alert(error));
+    };
 
-  const deleteNote = (id) => {
-    api
-      .delete(`/api/notes/delete/${id}/`)
-      .then((res) => {
-        if (res.status === 204) {
-          alert("Note deleted!");
-          getNotes();
-        } else {
-          alert("Failed to delete note.");
-        }
-      })
-      .catch((err) => alert(err.message));
-  };
+    const createNote = (e) => {
+        e.preventDefault();
+        api
+            .post("/api/notes/", { content, title })
+            .then((res) => {
+                if (res.status === 201) alert("Note created!");
+                else alert("Failed to make note.");
+                getNotes();
+            })
+            .catch((err) => alert(err));
+    };
 
-  const createNote = (e) => {
-    e.preventDefault();
-    api
-      .post("/api/notes/", { content, title })
-      .then((res) => {
-        if (res.status === 201) {
-          alert("Note created!");
-          getNotes();
-        } else {
-          alert("Failed to create note.");
-        }
-      })
-      .catch((err) => alert(err.message));
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div>
-        <h2>Notes</h2>
-        {notes.map((note) => (
-          <Note note={note} onDelete={deleteNote} key={note.id} />
-        ))}
-      </div>
-      <h2>Create a Note</h2>
-      <form onSubmit={createNote}>
-        <label htmlFor="title">Title:</label>
-        <br />
-        <input
-          type="text"
-          id="title"
-          name="title"
-          required
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
-        <label htmlFor="content">Content:</label>
-        <br />
-        <textarea
-          id="content"
-          name="content"
-          required
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </ThemeProvider>
-  );
-};
+    return (
+        <div>
+            <div>
+                <h2>Notes</h2>
+                {notes.map((note) => (
+                    <Note note={note} onDelete={deleteNote} key={note.id} />
+                ))}
+            </div>
+            <h2>Create a Note</h2>
+            <form onSubmit={createNote}>
+                <label htmlFor="title">Title:</label>
+                <br />
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    required
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
+                <label htmlFor="content">Content:</label>
+                <br />
+                <textarea
+                    id="content"
+                    name="content"
+                    required
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
+        </div>
+    );
+}
 
 export default Home;
